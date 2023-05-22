52e7263be9aba6c4d15afce6b6e8b7c090890002
IGORSERGEY <hewas2011@yandex.ru>
2023-05-22 21:43:18 +0300

    Added negative test data

--- unit/test-data/registration_form.test-data.ts
+++ unit/test-data/registration_form.test-data.ts
@@ -1,6 +1,8 @@
 import { expectedValidationResult } from '../helpers/constants';
 import { RandomGenerator } from '../helpers/random_values_generator';
+import { ERROR_MESSAGES } from '../helpers/types';
 
+export const ERROR_FOR_ALL_INVALID = `${ERROR_MESSAGES.INVALID_NAME}\n${ERROR_MESSAGES.INVALID_SURNAME}\n${ERROR_MESSAGES.INVALID_EMAIL}\n${ERROR_MESSAGES.INVALID_PASSWORD}\n${ERROR_MESSAGES.INVALID_AGE}\n${ERROR_MESSAGES.INVALID_PATRONYMIC}`;
 export const positiveTestData = [
     {
         testName: 'All fields are valid',
@@ -35,6 +37,17 @@ export const positiveTestData = [
             password: RandomGenerator.validPassword,
             age: RandomGenerator.validAge,
             patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "Patronymic" field is not provided',
+        registrationFormData: {
+            name: RandomGenerator.shortValidFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
         },
         expectedResult: expectedValidationResult,
     },
@@ -101,15 +114,70 @@ export const positiveTestData = [
 ];
 export const negativeTestData = [
     {
-        testName: '',
+        testName: 'Empty fields',
         registrationFormData: {
             name: '',
             surName: '',
             email: '',
             password: '',
             age: 1,
-            patronymic: '',
+            patronymic: '1',
         },
-        expectedResult: '',
+    },
+    {
+        testName: 'The name contains digits',
+        registrationFormData: {
+            name: 'Igor123',
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+    },
+    {
+        testName: 'The incorrect email',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: '123123@',
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+    },
+    {
+        testName: 'The age is 17',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: 17,
+            patronymic: RandomGenerator.validFullName,
+        },
+    },
+    {
+        testName: 'The age is 121',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.shortInvalidPassword,
+            age: 121,
+            patronymic: RandomGenerator.validFullName,
+        },
+    },
+    {
+        testName: 'The password length is 7 symbols',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.shortInvalidPassword,
+            age: 25,
+            patronymic: RandomGenerator.validFullName,
+        },
     },
 ];
+
