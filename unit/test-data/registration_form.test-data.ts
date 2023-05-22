fc9b6687d3d3bad5cf9e94d6580e6f5ce23336e8
IGORSERGEY <hewas2011@yandex.ru>
2023-05-22 19:34:15 +0300

    Added test data

--- unit/test-data/registration_form.test-data.ts
+++ unit/test-data/registration_form.test-data.ts
@@ -1,3 +1,6 @@
+import { expectedValidationResult } from '../helpers/constants';
+import { RandomGenerator } from '../helpers/random_values_generator';
+
 export const positiveTestData = [
     {
         testName: 'All fields are valid',
@@ -5,11 +8,95 @@ export const positiveTestData = [
             name: 'Igor',
             surName: 'Sergey',
             email: 'example@ex.com',
-            password: '123Qwerty$$',
+            password: '123Qwerty',
             age: 18,
             patronymic: 'Dmitrievich',
         },
-        expectedResult: 'Validation complete successful!',
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'All fields are random',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "name" field length is 1 letter',
+        registrationFormData: {
+            name: RandomGenerator.shortValidFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "surName" field length is 1 letter',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.shortValidFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "surName" field length is 32 letters',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.longValidFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "name" field length is 32 letters',
+        registrationFormData: {
+            name: RandomGenerator.longValidFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.validPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "password" field is 8 symbols',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.shortValidPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
+    },
+    {
+        testName: 'The "password" field is 32 symbols',
+        registrationFormData: {
+            name: RandomGenerator.validFullName,
+            surName: RandomGenerator.validFullName,
+            email: RandomGenerator.validEmail,
+            password: RandomGenerator.longValidPassword,
+            age: RandomGenerator.validAge,
+            patronymic: RandomGenerator.validFullName,
+        },
+        expectedResult: expectedValidationResult,
     },
 ];
 export const negativeTestData = [
