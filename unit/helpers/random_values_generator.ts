b7f21905caec29b28b89d03ac833df57a9e66ea2
IGORSERGEY <hewas2011@yandex.ru>
2023-05-22 19:30:08 +0300

    Added random values generator for test

---
+++ unit/helpers/random_values_generator.ts
@@ -0,0 +1,38 @@
+import RandExp from 'randexp';
+import { RegistrationForm } from '../classes/registration_form';
+
+const maxAge = RegistrationForm.maxAge;
+const minAge = RegistrationForm.minAge;
+
+export class RandomGenerator {
+    static get validFullName() {
+        return new RandExp(/^[A-Za-z]{1,32}$/).gen();
+    }
+    static get validEmail() {
+        return new RandExp(/^[\w-\.]+@([\w-]+\.)+[^\s|\d@]{2,4}$/).gen();
+    }
+    static get validPassword() {
+        return new RandExp(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@$!%^*?&])(1)[A-Za-z0-9@$!%+^*?&]{8,31}$/
+        ).gen();
+    }
+    static get validAge() {
+        return Math.floor(Math.random() * (maxAge - minAge) + minAge);
+    }
+    static get longValidFullName() {
+        return new RandExp(/^[A-Za-z]{32}$/).gen();
+    }
+    static get shortValidFullName() {
+        return new RandExp(/^[A-Za-z]{1}$/).gen();
+    }
+    static get longValidPassword() {
+        return new RandExp(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@$!%^*?&])(1)[A-Za-z0-9@$!%+^*?&]{31}$/
+        ).gen();
+    }
+    static get shortValidPassword() {
+        return new RandExp(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@$!%^*?&])(1)[A-Za-z0-9@$!%+^*?&]{7}$/
+        ).gen();
+    }
+}
