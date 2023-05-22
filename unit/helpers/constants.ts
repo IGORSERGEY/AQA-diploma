ec94e7e444aa07bacfa2b17d9011f7ec7d6f0674
IGORSERGEY <hewas2011@yandex.ru>
2023-05-22 13:36:18 +0300

    Added max and min length of name

--- unit/helpers/constants.ts
+++ unit/helpers/constants.ts
@@ -1,5 +1,5 @@
 export const regex = {
     password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d!@#$%^&*()_+]{8,32}$/,
-    fullName: /^[A-Za-z]+$/,
+    fullName: /^[A-Za-z]{1,32}$/,
     email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
 };
