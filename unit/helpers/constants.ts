f5b44d75afd5254aac5c3450f4a56f1d0048a0e2
IGORSERGEY <hewas2011@yandex.ru>
2023-05-22 19:30:39 +0300

    Changed password regexp and added const for expected result

--- unit/helpers/constants.ts
+++ unit/helpers/constants.ts
@@ -1,5 +1,6 @@
 export const regex = {
-    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d!@#$%^&*()_+]{8,32}$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[A-Za-z0-9@$!%^*?&])[A-Za-z0-9@$!%+^*?&]{8,32}$/,
     fullName: /^[A-Za-z]{1,32}$/,
-    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    email: /^[\w-\.]+@([\w-]+\.)+[^\s|\d@]{2,4}$/,
 };
+export const expectedValidationResult = 'Validation complete successful!';
