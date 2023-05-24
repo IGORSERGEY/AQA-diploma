420c92289309c00be60e8523178035e056b0aa08
IGORSERGEY <hewas2011@yandex.ru>
2023-05-24 19:46:46 +0300

    Added log message "schema is valid!"

--- api/helpers/schema_validator.ts
+++ api/helpers/schema_validator.ts
@@ -7,6 +7,7 @@ export function validateSchema(schema: any, data: any) {
     try {
         apiLogger.info(`Validating schema: \n${JSON.stringify(schema)} \n with data: \n${JSON.stringify(data)}`);
         expect(ajv.validate(schema, data)).toBeTruthy();
+        apiLogger.info('Schema is valid !');
     } catch (err) {
         apiLogger.error(`Schema validation error ${ajv.errorsText()}`);
         throw new Error(ajv.errorsText());
