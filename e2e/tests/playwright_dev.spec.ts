ed0083aa0a8f527aca7f4d664a68b87b51270c1e
IGORSERGEY <hewas2011@yandex.ru>
2023-05-24 16:05:34 +0300

    Added "Tests for docks page" describe Changed tests order it "Tests for playwright.dev home page" describe Renamed import

--- e2e/tests/playwright_dev.spec.ts
+++ e2e/tests/playwright_dev.spec.ts
@@ -2,11 +2,13 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../src/home_page';
 import { PageFactory } from '../src/page_factory';
 import { PAGES } from '../helpers/types';
-import { partnerLinks } from '../helpers/constants';
+import { HEADER_SECTIONS, PARTNERS_LINKS, RIGHT_MENU_SECTIONS } from '../helpers/constants';
+import { DocsPage } from '../src/docs_page';
 
 test.describe.configure({ mode: 'serial' });
 
 let homePage: HomePage;
+let docsPage: DocsPage;
 
 test.describe('Tests for playwright.dev home page', () => {
     test.beforeAll(async ({ browser }) => {
@@ -14,9 +16,46 @@ test.describe('Tests for playwright.dev home page', () => {
         homePage = PageFactory.getPage(page, PAGES.HOME) as HomePage;
         await homePage.openPage();
     });
+    test('Can toggle theme with button', async () => {
+        const themeBeforeToggle = await homePage.getTheme();
+        await homePage.toggleTheme();
+        const themeAfterToggle = await homePage.getTheme();
+        expect(themeBeforeToggle).not.toBe(themeAfterToggle);
+    });
     test('The partners links are correct:', async () => {
-        for (let [index, link] of partnerLinks.entries()) {
+        for (let [index, link] of PARTNERS_LINKS.entries()) {
             expect(await homePage.getPartnersButtonLinksByNumber(index + 1)).toEqual(link);
         }
     });
+    test('Can open search field by hotkeys', async () => {
+        await homePage.performSearch();
+        expect(await homePage.isSearchBarVisible()).toBeTruthy();
+    });
 });
+
+test.describe('Tests for docks page', () => {
+    test.beforeAll(async ({ browser }) => {
+        const page = await browser.newPage();
+        docsPage = PageFactory.getPage(page, PAGES.DOCKS) as DocsPage;
+        await docsPage.openPage();
+    });
+    for (let [section, menuItem] of HEADER_SECTIONS) {
+        test(`Can open ${section} section and see ${menuItem} left menu item`, async () => {
+            await docsPage.clickHeaderButtonByText(section);
+            expect(await docsPage.getLeftMenuItemByText(menuItem)).toBeVisible();
+        });
+    }
+    for (let [rightMenuSection, leftMenuSections] of RIGHT_MENU_SECTIONS) {
+        test(`The "${rightMenuSection}" section contains "${leftMenuSections
+            .toString()
+            .split(',')
+            .join(', ')} table of contents"`, async () => {
+            await (await docsPage.getLeftMenuItemByText(rightMenuSection as string)).click();
+            for (let section of leftMenuSections) {
+                expect(await docsPage.getRightMenuItemByText(section)).toBeVisible();
+            }
+        });
+    }
+});
+
+//
