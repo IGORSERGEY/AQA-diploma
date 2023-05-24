8d3a1394e797e17bf8ed8eeae0651a7fd0796fee
IGORSERGEY <hewas2011@yandex.ru>
2023-05-24 15:15:46 +0300

    Added toggleTheme and getTheme methods to basePage object

--- e2e/src/base_page.ts
+++ e2e/src/base_page.ts
@@ -6,11 +6,20 @@ export class BasePage {
     public async openPage() {
         await this.page.goto(this.Url);
     }
-    public async performSearch(textToSearch: string) {
+    public async performSearch(textToSearch?: string) {
         await this.page.keyboard.press('Control+K');
-        await this.page.keyboard.type(textToSearch, { delay: 50 });
+        textToSearch ? await this.page.keyboard.type(textToSearch, { delay: 50 }) : await this.page.waitForTimeout(100);
+    }
+    public async isSearchBarVisible() {
+        return await this.page.locator('header.DocSearch-SearchBar').isVisible();
     }
     public async clickHeaderButtonByText(buttonText: string) {
         await this.page.locator('a.navbar__item ').getByText(buttonText).click();
+    }
+    public async toggleTheme() {
+        await this.page.locator('svg.lightToggleIcon_pyhR').click();
+    }
+    public async getTheme() {
+        return await this.page.locator('html[class="plugin-pages plugin-id-default"]').getAttribute('data-theme');
     }
 }
