bd8310517b817c2a6b70e6433e1d9da3fcf8005a
IGORSERGEY <hewas2011@yandex.ru>
2023-05-24 15:15:18 +0300

    Added RIGHT_MENU_ITEMS map

--- e2e/helpers/constants.ts
+++ e2e/helpers/constants.ts
@@ -15,3 +15,55 @@ export const HEADER_SECTIONS = [
     ['Community', 'Welcome'],
     ['Docs', 'Writing tests'],
 ];
+
+export const RIGHT_MENU_SECTIONS = [
+    ['Installation', ['Installing Playwright', "What's Installed", 'Running the Example Test', 'HTML Test Reports']],
+    [
+        'Writing tests',
+        [
+            'First test',
+            'Actions',
+            'Navigation',
+            'Interactions',
+            'Basic actions',
+            'Test Isolation',
+            'Using Test Hooks',
+            "What's Next",
+        ],
+    ],
+    ['Running tests', ['Run tests in UI Mode', 'Command Line', 'Debugging Tests', 'Test Reports', "What's Next"]],
+    ['Test generator', ['Running Codegen', 'Recording a test', 'Generating locators', 'Emulation', "What's Next"]],
+    ['Trace viewer', ['Recording a Trace', 'Opening the HTML Report', 'Viewing the Trace']],
+    [
+        'UI Mode',
+        [
+            'Running tests in UI Mode',
+            'Filtering tests',
+            'Running your tests',
+            'Viewing test traces',
+            'Actions and metadata',
+            'Source, console, log and network',
+            'Pop out and inspect the DOM',
+            'Timeline view',
+            'Pick locator',
+            'Watch mode',
+        ],
+    ],
+    [
+        'CI GitHub Actions',
+        [
+            'Create a Repo and Push to GitHub',
+            'Opening the Workflows',
+            'Viewing Test Logs',
+            'HTML Report',
+            'Downloading the HTML Report',
+            'Viewing the HTML Report',
+            'Viewing the Trace',
+            "What's Next",
+        ],
+    ],
+    // ['Installation', ['API reference']],
+    // ['Installation', ['API reference']],
+    // ['Installation', ['API reference']],
+    // ['Installation', ['API reference']],
+];
