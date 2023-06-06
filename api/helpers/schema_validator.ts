e6a1d7d97f7702c09e1a8aa4e6f97373ecc1f819
IGORSERGEY <hewas2011@yandex.ru>
2023-06-06 21:19:47 +0300

    Removed logging code from test body

--- api/helpers/schema_validator.ts
+++ api/helpers/schema_validator.ts
@@ -1,15 +1,12 @@
 import Ajv from 'ajv';
-import { apiLogger } from '../config/logger.config';
+// import { apiLogger } from '../config/logger.config';
 
 const ajv = new Ajv();
 
 export function validateSchema(schema: any, data: any) {
     try {
-        apiLogger.info(`Validating schema: \n${JSON.stringify(schema)} \n with data: \n${JSON.stringify(data)}`);
         expect(ajv.validate(schema, data)).toBeTruthy();
-        apiLogger.info('Schema is valid !');
     } catch (err) {
-        apiLogger.error(`Schema validation error ${ajv.errorsText()}`);
         throw new Error(ajv.errorsText());
     }
 }
--- api/tests/reqres.spec.ts
+++ api/tests/reqres.spec.ts
@@ -4,21 +4,18 @@ import * as userListSchema from '../schema/user_list_schema.json';
 import * as userSchema from '../schema/user_schema.json';
 import * as registrationResponseSchema from '../schema/registration_response_schema.json';
 import { RequestBodyGenerator, randomUserIdNumber } from '../helpers/request_body_generator';
-import { apiLogger } from '../config/logger.config';
 import { reqres } from '../config/axios.config';
 
 describe('Positive API tests for reqres.in', () => {
     const userId = randomUserIdNumber;
 
     it('Should get users list', async () => {
-        apiLogger.info(`Sending request [GET] ${baseUrl}/users ...`);
         let response = await reqres.get('/users', {
             params: {
                 page: usersPage,
                 per_page: maxUserId,
             },
         });
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
         expect(response.status).toBe(200);
         validateSchema(userListSchema, response.data);
         expect(response.data.per_page).toBe(maxUserId);
@@ -27,26 +24,20 @@ describe('Positive API tests for reqres.in', () => {
 
     it('Should create user', async () => {
         const requestBody = RequestBodyGenerator.getUserToCreate();
-        apiLogger.info(`Sending request [POST] ${baseUrl}/users with body: \n${JSON.stringify(requestBody)}`);
         let response = await reqres.post(baseUrl + '/users', requestBody);
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
 
         expect(response.status).toBe(201);
         expect(response.data).toMatchObject(requestBody);
     });
     it(`Should find user №${userId} by id`, async () => {
-        apiLogger.info(`Sending request [GET] ${baseUrl}/users/${userId} ...`);
         let response = await reqres.get(baseUrl + `/users/${userId}`);
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
 
         expect(response.data.data.id).toBe(userId);
         validateSchema(userSchema, response.data);
     });
     it(`Should update user №${userId}`, async () => {
         const requestBody = RequestBodyGenerator.getUserForUpdate(userId);
-        apiLogger.info(`Sending request [PUT] ${baseUrl}/users/${userId} with body: \n${JSON.stringify(requestBody)}`);
         let response = await reqres.put(baseUrl + `/users/${userId}`, requestBody);
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
 
         expect(response.status).toBe(200);
         validateSchema(userSchema, response.data);
@@ -54,13 +45,9 @@ describe('Positive API tests for reqres.in', () => {
         expect(response.data).toMatchObject(requestBody);
     });
     it(`Should update user's №${userId} email`, async () => {
-        apiLogger.info(`Sending request [GET] ${baseUrl}/users/${userId} ...`);
         let responseBefore = await reqres.get(baseUrl + `/users/${userId}`);
-        apiLogger.info(`The response before patching is:\n${JSON.stringify(responseBefore.data)}`);
         const requestBody = RequestBodyGenerator.getUserForUpdate(userId);
-        apiLogger.info(`Sending request [PATCH] ${baseUrl}/users with body: \n${JSON.stringify(requestBody)}`);
         let responseAfter = await reqres.patch(baseUrl + `/users/${userId}`, requestBody);
-        apiLogger.info(`The response after patching is:\n${JSON.stringify(responseAfter.data)}`);
 
         expect(responseAfter.status).toBe(200);
         validateSchema(userSchema, responseAfter.data);
@@ -70,16 +57,12 @@ describe('Positive API tests for reqres.in', () => {
     });
 
     it(`Should delete user №${userId}`, async () => {
-        apiLogger.info(`Sending request [DELETE] ${baseUrl}/users/${userId} ...`);
         let response = await reqres.delete(baseUrl + `/users/${userId}`);
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
         expect(response.status).toBe(204);
     });
     it('Should register a user', async () => {
         const requestBody = RequestBodyGenerator.getUserCredentials();
-        apiLogger.info(`Sending request [POST] ${baseUrl}/users/${userId} with body: \n${JSON.stringify(requestBody)}`);
         let response = await reqres.post(baseUrl + '/register', requestBody);
-        apiLogger.info(`The response is:\n${JSON.stringify(response.data)}`);
         expect(response.status).toBe(200);
         validateSchema(registrationResponseSchema, response.data);
     });
@@ -88,10 +71,8 @@ describe('Negative tests for reqres.in', () => {
     it(`Registration: Should give an error '${missingPasswordMessage}' when password wasn't provided`, async () => {
         try {
             const requestBody = RequestBodyGenerator.getUserCredentialsWithoutPassword();
-            apiLogger.info(`Sending request [POST] ${baseUrl}/register with body: \n${JSON.stringify(requestBody)}`);
             await reqres.post(baseUrl + '/register', requestBody);
         } catch (error: any) {
-            apiLogger.info(`The response is:\n${JSON.stringify(error.response.data)}`);
             expect(error.response.status).toBe(400);
             expect(error.response.data.error).toBe(missingPasswordMessage);
         }
@@ -99,20 +80,16 @@ describe('Negative tests for reqres.in', () => {
     it(`Login: Should give an error '${missingPasswordMessage}' when password wasn't provided`, async () => {
         try {
             const requestBody = RequestBodyGenerator.getUserCredentialsWithoutPassword();
-            apiLogger.info(`Sending request [POST] ${baseUrl}/register with body: \n${JSON.stringify(requestBody)}`);
             await reqres.post(baseUrl + '/login', requestBody);
         } catch (error: any) {
-            apiLogger.info(`The response is:\n${JSON.stringify(error.response.data)}`);
             expect(error.response.status).toBe(400);
             expect(error.response.data.error).toBe(missingPasswordMessage);
         }
     });
     it('Should get 404 error when trying to find nonexistent user', async () => {
         try {
-            apiLogger.info(`Sending request [GET] ${baseUrl}/users/${nonExistUser}`);
             await reqres.get(baseUrl + `/users/${nonExistUser}`);
         } catch (error: any) {
-            apiLogger.info(`The response is:\n${JSON.stringify(error.response.data)}`);
             expect(error.response.status).toBe(404);
         }
     });
