8287aa7457422767a8ea8f71840d6b665a31db2d
IGORSERGEY <hewas2011@yandex.ru>
2023-06-06 21:16:06 +0300

    Added axios-logger

--- api/config/axios.config.ts
+++ api/config/axios.config.ts
@@ -1,7 +1,33 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { baseUrl } from '../test-data/constants';
+import * as AxiosLogger from 'axios-logger';
+import { apiLogger } from './logger.config';
 
 export const reqres = axios.create({
     baseURL: baseUrl,
-    timeout: 1000,
+    timeout: 2000,
 });
+
+const axiosLoggerConfig = {
+    prefixText: 'INFO',
+    dateFormat: '[dd/MM/yyyy] [hh:mm:ss]',
+};
+
+reqres.interceptors.request.use(function (request) {
+    apiLogger.info(`Request: ${request.method} ${request.url} ${request.data ? JSON.stringify(request.data) : ''}`);
+    AxiosLogger.requestLogger(request, axiosLoggerConfig);
+    return request;
+});
+reqres.interceptors.response.use(
+    function (response) {
+        apiLogger.info(`Response: ${JSON.stringify(response.data)}`);
+        return AxiosLogger.responseLogger(response, axiosLoggerConfig);
+    },
+    function (error) {
+        apiLogger.error(`${error.message}`);
+        return AxiosLogger.errorLogger(error, {
+            ...axiosLoggerConfig,
+            prefixText: 'ERROR',
+        });
+    }
+);
